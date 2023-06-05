498cb0f501627b9ef8a61ce1b8738f21a2fb0bef
pier <pierknows@gmail.com>
2023-06-05 10:11:27 -0400

    This commit will show the variables but its WIP, when selecting the answer it doesnt select it and refreshes the page

--- js/scripts.js
+++ js/scripts.js
@@ -15,9 +15,18 @@ window.onload = function() {
       let questionThree = document.querySelector("div#questionThree");
       let questionFour = document.querySelector("div#questionFour");
       let questionFive = document.querySelector("div#questionFive");
-    
 
-      
+        
+    if (questionOne === "C" && questionTwo === "Purple and white" && questionThree === "Easy" && questionFour === "Man" && questionThree === "Mornings") {
+        document.getElementById("C-#").removeAttribute("class");
+    }
+    else if (questionOne === "J" && questionTwo === "Black and Yellow" && questionThree === "Intermediate" && questionFour === "Computer" && questionThree === "Afternoons") {
+        document.getElementById("Javascript").removeAttribute("class");
+    } 
+    else if (questionOne === "P" && questionTwo === "Light Blue and White" && questionThree === "Difficult" && questionFour === "Snake" && questionThree === "Evenings") {
+        document.getElementById("Python").removeAttribute("class");
 
     }
+}
+
 }
