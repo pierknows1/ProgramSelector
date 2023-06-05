916451c1bdaeab57f0048d05e81c92de454f83cd
pier <pierknows@gmail.com>
2023-06-05 06:56:45 -0400

    This commit will have all questions, images for the answers, have the hidden button for the images, added the method to getelementbyId

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+function logoHidden(){
+    document.getElementById("c-#").setAttribute("class", "hidden");
+    document.getElementById("Javascript").setAttribute("class", "hidden");
+    document.getElementById("Python").setAttribute("class", "hidden");
+}
