78e31ec3701b53885224dab28b1619cf3545ad2a
pier <pierknows@gmail.com>
2023-06-05 09:34:07 -0400

    this commit will have labels and some debugging in the JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
 function logoHidden(){
-    document.getElementById("c-#").setAttribute("class", "hidden");
+    document.getElementById("C-#").setAttribute("class", "hidden");
     document.getElementById("Javascript").setAttribute("class", "hidden");
     document.getElementById("Python").setAttribute("class", "hidden");
+}
+
+window.onload = function() {
+    hideResults();
+    document.querySelector("form").onsubmit = function(event) {
+      event.preventDefault();
+      hideResults();
+
+      let questionOne = document.querySelector("div#questionOne");
+      let questionTwo = document.querySelector("div#questionTwo");
+      let questionThree = document.querySelector("div#questionThree");
+      let questionFour = document.querySelector("div#questionFour");
+      let questionFive = document.querySelector("div#questionFive");
+    
+
+      
+
+    }
 }
