c7399e771318f2fabf4b9b4050a905644bb5cb37
pier <pierknows@gmail.com>
2023-06-10 21:07:08 -0400

    committing updated version so i can open up new version on my pc

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,23 @@
 function logoHidden(){
-    document.getElementById("C-#").setAttribute("class", "hidden");
+    document.getElementById("C-sharp").setAttribute("class", "hidden");
     document.getElementById("Javascript").setAttribute("class", "hidden");
     document.getElementById("Python").setAttribute("class", "hidden");
 }
 
-window.onload = function() {
-    hideResults();
-    document.querySelector("form").onsubmit = function(event) {
-      event.preventDefault();
-      hideResults();
+function submitResult(event){
+    event.preventDefault();
+    logoHidden();
 
-      let questionOne = document.querySelector("div#questionOne");
-      let questionTwo = document.querySelector("div#questionTwo");
-      let questionThree = document.querySelector("div#questionThree");
-      let questionFour = document.querySelector("div#questionFour");
-      let questionFive = document.querySelector("div#questionFive");
+
+      let questionOne = document.querySelector("div#questionOne")
+      let questionTwo = document.querySelector("div#questionTwo")
+      let questionThree = document.querySelector("div#questionThree")
+      let questionFour = document.querySelector("div#questionFour")
+      let questionFive = document.querySelector("div#questionFive")
 
         
     if (questionOne === "C" && questionTwo === "Purple and white" && questionThree === "Easy" && questionFour === "Man" && questionThree === "Mornings") {
-        document.getElementById("C-#").removeAttribute("class");
+        document.getElementById("C-sharp").removeAttribute("class");
     }
     else if (questionOne === "J" && questionTwo === "Black and Yellow" && questionThree === "Intermediate" && questionFour === "Computer" && questionThree === "Afternoons") {
         document.getElementById("Javascript").removeAttribute("class");
@@ -27,6 +26,9 @@ window.onload = function() {
         document.getElementById("Python").removeAttribute("class");
 
     }
+
 }
 
+window.onload = function() {
+    document.querySelector("form").onsubmit = submitResult
 }
